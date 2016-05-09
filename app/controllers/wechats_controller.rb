257b6009503cfd9458e40ced826e35014f5f7f5a
Wells <kejincan@sensetime.com>
2016-05-09 12:53:30 +0800

    Fix wechat/wx_valid-render-error

--- app/controllers/wechats_controller.rb
+++ app/controllers/wechats_controller.rb
@@ -2,5 +2,6 @@ class WechatsController < ApplicationController
 
 	def wx_valid
 		Wechat.check_wx_signature(params[:timestamp], params[:nonce], params[:signature])
+		render plain: params[:echostr]
 	end
 end
