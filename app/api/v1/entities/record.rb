9afcbb695a12a0518777f536dd0843377f2a191f
Wells <kejincan@sensetime.com>
2016-05-13 01:06:45 +0800

    Improve page-of-profile

--- app/api/v1/entities/record.rb
+++ app/api/v1/entities/record.rb
@@ -1,7 +1,7 @@
 	module V1
 		module Entities
 			class Record < Grape::Entity
-				expose :id, documentation: { type: Integer, desc: 'id of record' }, as: :record_id
+				expose :url, documentation: { type: String, desc: 'url of record' }
 			end
 		end
 	end
